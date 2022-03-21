1f9a4cf749f55b4fbf8e94db575770d0e0df7988
Milan Ivic <ivicmilan050@gmail.com>
2022-03-21 12:06:59 +0100

    added main class

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import domain.Person;
+
+public class Main {
+	public static void main(String[]args) {
+		Person person = new Person("Milan", "Ivic",31);
+		
+		System.out.println("Name: "+person.getName()+" \nSurname: "+person.getSurname()+" \nAge: "+person.getAge());
+	}
+}
